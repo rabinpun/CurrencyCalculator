0364a588beed56f957bd91887cc13313770db56c
rabin.pun <rabin.pun@ebpearls.com>
2022-08-28 20:20:23 +0545

    add bottom line in textfield

--- CurrencyConverter/Modules/Home/Converter/Views/TextField.swift
+++ CurrencyConverter/Modules/Home/Converter/Views/TextField.swift
@@ -10,6 +10,13 @@ import UIKit
 final class TextField: UITextField {
     
     let padding = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+    
+    override func draw(_ rect: CGRect) {
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0.0, y: rect.height - 1, width: rect.width, height: 1.0)
+        bottomLine.backgroundColor = UIColor.black.cgColor
+        layer.addSublayer(bottomLine)
+    }
 
     override public func textRect(forBounds bounds: CGRect) -> CGRect {
             return bounds.inset(by: padding)
