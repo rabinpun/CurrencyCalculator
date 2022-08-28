8c2bb61947a21a3c6ce362671dfa767c870e62b9
rabin.pun <rabin.pun@ebpearls.com>
2022-08-28 10:47:57 +0545

    add gradient to views

--- CurrencyConverter/Modules/Home/Converter/ConverterView.swift
+++ CurrencyConverter/Modules/Home/Converter/ConverterView.swift
@@ -9,6 +9,8 @@ import UIKit
 
 final class ConverterView: BaseView {
     
+    private var viewsLayedout = false
+    
     lazy var amountTextField: TextField = {
         let textField = TextField(frame: .zero)
         textField.layer.borderWidth = 1
@@ -39,9 +41,12 @@ final class ConverterView: BaseView {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collectionView.backgroundColor = .clear
         collectionView.showsVerticalScrollIndicator = false
+//        collectionView.backgroundColor = .clear
         collectionView.bounces = false
         collectionView.contentInsetAdjustmentBehavior = .never
         collectionView.contentInset = .zero
+        collectionView.maskTop(40)
+        collectionView.contentInset = UIEdgeInsets(top: 20, left: 5, bottom: 0, right: 5)
         collectionView.register(ConversionResultCell.self, forCellWithReuseIdentifier: ConversionResultCell.identifier)
         collectionView.register(CurrencyErrorCell.self, forCellWithReuseIdentifier: CurrencyErrorCell.identifier)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -69,8 +74,19 @@ final class ConverterView: BaseView {
         return toolBar
     }()
     
+    override func layoutSubviews() {
+        guard !viewsLayedout, conversionCollectionView.frame.minY > 0 else { return }
+        viewsLayedout.toggle()
+        addGradient([.appBlue, .appGreen], locations: [0, 1], frame: CGRect(x: 0, y: 0, width: frame.width, height: conversionCollectionView.frame.minY + 50))
+        
+        let view = UIView()
+        let gradientFrame = CGRect(x: 0, y: 0, width: conversionCollectionView.bounds.width, height: conversionCollectionView.bounds.height)
+        view.addGradient([.appGreen, .appBlue], locations: [0, 1], frame: gradientFrame)
+        conversionCollectionView.backgroundView = view
+    }
+    
     override func create() {
-        backgroundColor = .appGreen
+        backgroundColor = .white
         addSubview(amountTextField)
         addSubview(selectedCurrencyView)
         addSubview(conversionCollectionView)
@@ -82,14 +98,14 @@ final class ConverterView: BaseView {
             amountTextField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -10),
             amountTextField.heightAnchor.constraint(equalToConstant: 50),
             
-            selectedCurrencyView.topAnchor.constraint(equalTo: amountTextField.bottomAnchor, constant: 10),
+            selectedCurrencyView.topAnchor.constraint(equalTo: amountTextField.bottomAnchor, constant: 20),
             selectedCurrencyView.leadingAnchor.constraint(equalTo: amountTextField.centerXAnchor),
             selectedCurrencyView.trailingAnchor.constraint(equalTo: amountTextField.trailingAnchor),
             selectedCurrencyView.heightAnchor.constraint(equalToConstant: 50),
             
-            conversionCollectionView.topAnchor.constraint(equalTo: selectedCurrencyView.bottomAnchor, constant: 10),
-            conversionCollectionView.leadingAnchor.constraint(equalTo: amountTextField.leadingAnchor),
-            conversionCollectionView.trailingAnchor.constraint(equalTo: amountTextField.trailingAnchor),
+            conversionCollectionView.topAnchor.constraint(equalTo: selectedCurrencyView.bottomAnchor, constant: 40),
+            conversionCollectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            conversionCollectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
             conversionCollectionView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
         ])
     }
